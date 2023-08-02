84b34d52d2d6013991c5cca874b47afafb4ecfa9
xx <1804113857@qq.com>
2023-08-02 10:44:25 +0800

    测试新分支2

--- AOP/src/main/java/com/yc/test/TestBranch.java
+++ AOP/src/main/java/com/yc/test/TestBranch.java
@@ -2,6 +2,6 @@ package com.yc.test;
 
 public class TestBranch {
     public static void main(String[] args) {
-        System.out.println("测试创建新分支....");
+        System.out.println("测试创建新分支.....");
     }
 }
